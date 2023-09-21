58bcaa7265b4803997b7439e9bd2787d52049f78
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-09-21 11:12:47 -0700

    add starter for module 2

---
+++ module-02/solution/SpaceInvaders/scripts/player.js
@@ -0,0 +1,15 @@
+class Player {
+  constructor({ x, y }) {
+    this.element = document.getElementById("player");
+    this.speed = 5;
+    x = x || 0;
+    y = y || 0;
+    this.positionElement(x, y)
+  }
+
+  positionElement(x, y) {
+    this.x = x;
+    this.y = y;
+    this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
+}
